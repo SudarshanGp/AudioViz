f3d766e5e985394789561e9bd3a62ce9b8246190
Sudarshan Govindaprasad <Sudarshan@Sudarshans-MacBook-Air.local>
2016-03-28 15:14:30 -0500

    updating repository with new data and code

--- analyze.py
+++ analyze.py
@@ -20,18 +20,10 @@ from tinytag import TinyTag
 plt.style.use('ggplot')
 
 data = pd.read_csv('data.csv', index_col=0)
-# data.replace([np.inf, -np.inf], np.nan)
-# print(data['2_differences_1_sample_skewness'])
 data = data.fillna(0)
-# print(data['2_differences_1_sample_skewness'])
-
-# data = data.replace(r'\s+', 0, regex=True)
-# print(data.info())
 pca = PCA(n_components=31).fit_transform(data.values)
-# print(pca)
-kmeans = cluster.KMeans(n_clusters=10)
+kmeans = cluster.KMeans(n_clusters=8)
 data1 = kmeans.fit_predict(pca)
-# print(data1)	
 index = 0
 mapping = {}
 for key, value in data.iterrows():
@@ -50,23 +42,20 @@ song_information = {}
 
 for path, dirs, files in os.walk('source/'):
 	for f in files:
-		# print(path)
-		# print(files)
-		# Check that the file is a WAV fil
 		if f.endswith('.mp3'):
 			temp_dict = {}
 			name = ''.join(f.split('.mp3')[-2])
 			tag = TinyTag.get(path + name + '.mp3')
-			# print(name + '.mp3')
 			for key, value in mapping.iteritems():
 				for i, v in enumerate(value):
 					if name + '.mp3' in v:
-						temp_dict['cluster'] = str(key)
+						print("old key", key)
+						temp_dict['cluster'] = str(key+1).zfill(2) 
+						print("new key", str(key+1).zfill(2) )
 						print(key)
-			# print(mapping[name + '.mp3'])
 			temp_dict['title'] = tag.title.encode('ascii','ignore')
 			if tag.genre is None:
-				temp_dict['genre'] = 'None'
+				temp_dict['genre'] = 'Other'
 			else:
 				temp_dict['genre'] = tag.genre.encode('ascii','ignore')
 			temp_dict['album'] = tag.album.encode('ascii','ignore')
@@ -77,20 +66,3 @@ pprint.pprint(song_information)
 print(type(song_information))
 with open('data.json', 'w') as outfile:
     json.dump(song_information, outfile)
-# viz_dict = {}
-# viz_dict['name'] = 'My Songs'
-# viz_dict['children'] = []
-# temp_children = []
-# for i in range(1,11):
-	
-# for key, value in song_information.iteritems():
-
-
-
-# kmeans.fit(x_train)
-# y = kmeans.predict(x_test)
-
-# x, y = zip(*pca)
-
-# plt.scatter(x, y)
-# plt.show()
--- visualize.py
+++ visualize.py
@@ -11,41 +11,104 @@ import pandas as pd
 import pprint
 import os
 import json
+import copy 
 
 with open('data.json') as data_file:    
     data = json.load(data_file)
-pprint.pprint(data)
 viz_dict = {}
 viz_dict['name'] = 'My Songs'
 viz_dict['children'] = []
 temp_children = []
-for i in range(1,11):
+for i in range(1,9):
 	new_cluster = {}
-	new_cluster['name'] = 'Cluster ' + str(i)
+	new_cluster['name'] = 'Cluster ' + str(i).zfill(2)
 	new_cluster['children'] = []
 	temp_children.append(new_cluster)
 viz_dict['children'] = temp_children
-# pprint.pprint(viz_dict)
-
+final_dict = copy.deepcopy(viz_dict)
 for key, value in data.iteritems():
-	print(key)
-	cluster_name = 'Cluster ' + str(int(value['cluster'])+1)
+	cluster_name = 'Cluster ' + value['cluster']
 	list_index = next(index for (index, d) in enumerate(viz_dict['children']) if d["name"] == cluster_name)
+	find_index = -1
+	for i, v in enumerate(viz_dict['children']):
+		if v['name'] == str(cluster_name):
+			find_index = i
+			break
 	temp_song = {}
 	temp_song = value
 	temp_song['name'] = value['title']
 	temp_song['size'] = 10
-	viz_dict['children'][list_index]['children'].append(temp_song)
+	viz_dict['children'][find_index]['children'].append(temp_song)
+copy_songs = {} # by cluster
 
-pprint.pprint(viz_dict)
+for key, value in enumerate(viz_dict['children']):
+	curr_cluster = key + 1
+	cluser_name = value['name']
+	curr_songs = copy.deepcopy(value['children'])
+	copy_songs[cluser_name] = curr_songs
 
+for key, value in copy_songs.iteritems():
+	# curr_cluster = key + 1
+	cluser_name = key
+	# curr_songs = copy.deepcopy(value['children'])
+	by_artist_temp = {}
+	by_artist_temp['name'] = 'Artists'
+	by_artist_temp['children'] = []
 
+	for i, v in enumerate(value):
+		found = False
+		for i1, v1 in enumerate(by_artist_temp['children']):
+			if v['artist'] in v1['name']:
+				found = True
+		if found is False:
+			temp_artist = {}
+			temp_artist['name'] = v['artist']
+			temp_artist['children'] = []
+			by_artist_temp['children'].append(temp_artist)
+	for i, v in enumerate(value):
+		for i1, v1 in enumerate(by_artist_temp['children']):
+			if v['artist'] in v1['name']:
+				v1['children'].append(v)
+	find_index = -1
+	for i, v in enumerate(final_dict['children']):
+		if v['name'] is key:
+			find_index = i
+			break;
 
-with open('flare1.json', 'w') as outfile:
-    json.dump(viz_dict, outfile)
-	# print(value)
+	final_dict['children'][i]['children'].append(by_artist_temp)
 
+for key, value in copy_songs.iteritems():
+	# curr_cluster = key + 1
+	cluser_name = key
+	# curr_songs = copy.deepcopy(value['children'])
+	by_artist_temp = {}
+	by_artist_temp['name'] = 'Genre'
+	by_artist_temp['children'] = []
 
+	for i, v in enumerate(value):
+		found = False
+		for i1, v1 in enumerate(by_artist_temp['children']):
+			if v['genre'] in v1['name']:
+				found = True
+		if found is False:
+			temp_artist = {}
+			temp_artist['name'] = v['genre']
+			temp_artist['children'] = []
+			by_artist_temp['children'].append(temp_artist)
+	for i, v in enumerate(value):
+		for i1, v1 in enumerate(by_artist_temp['children']):
+			if v['genre'] in v1['name']:
+				v1['children'].append(v)
+	find_index = -1
+	for i, v in enumerate(final_dict['children']):
+		if v['name'] is key:
+			find_index = i
+			break;
+
+	final_dict['children'][i]['children'].append(by_artist_temp)
+
+with open('visual.json', 'w') as outfile:
+    json.dump(final_dict, outfile)
 
 
 
